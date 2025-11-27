387e069855727f266f5c51b3ee0724ddcf3eb324
ROMANGREKHOV <94071555+ROMANGREKHOV@users.noreply.github.com>
2025-11-27 21:23:13 +0300

    DZ5_1.cpp

--- DZ5_1.cpp
+++ DZ5_1.cpp
@@ -1,14 +1,15 @@
-//#include <iostream>
-//
-//int main() {
-//	int arr[] { 1,2,3,4,5,6,7,8,9,10 };
-//	for (int i = 0; i < 10; i++) {
-//		if (i == 9) {
-//			std::cout << arr[i];
-//			break;
-//		}
-//		std::cout << arr[i] << ", ";
-//	}
-//
-//	return EXIT_SUCCESS;
-//}
+#include <iostream>
+
+int main() {
+	int arr[] { 1,2,3,4,5,6,7,8,9,10 };
+	for (int i = 0; i < 10; i++) {
+		if (i == 9) {
+			std::cout << arr[i];
+			break;
+		}
+		std::cout << arr[i] << ", ";
+	}
+
+	return EXIT_SUCCESS;
+
+}
